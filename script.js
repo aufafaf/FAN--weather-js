c961be5294d5adad1df11a3ada230e699bee50bf
aufafaf <mfaufalahatx@gmail.com>
2026-01-26 20:45:08 +0700

    add js

--- script.js
+++ script.js
@@ -1,18 +1,45 @@
-const fetchWeather = async (city) => {
-  try {
-    const response = await fetch(
-      "https://api.openweathermap.org/data/2.5/weather?q=Jakarta&appid=API_KEY&units=metric",
-    );
+const API_KEY = "95858a8993ce99df6fd91d338fbb67c0";
+const API_URL = "https://api.openweathermap.org/data/2.5/weather";
 
-    if (!response.ok) {
-      throw new Error("City not found");
-    }
+const inputForm = document.getElementById("input-form");
+const inputButton = document.getElementById("search-btn");
+const loading = document.getElementById("loading");
+const error = document.getElementById("error");
+const weather = document.getElementById("displayWeather");
 
-    const data = await response.json();
+console.log("input form :", inputForm);
+console.log("search button :", inputButton);
+console.log("loading :", loading);
+console.log("error :", error);
+console.log("weather :", weather);
 
-    const temp = data?.main?.temp;
-    console.log(`Temperature: ${temp}K`);
-  } catch (error) {
-    console.log("Error:", error.message);
+const weatherForm = document.getElementById("form");
+
+weatherForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const city = inputForm.value.trim();
+
+  if (!city) {
+    error.textContent = "Nama kota tidak boleh kosong";
+    error.classList.remove("hidden");
+    return;
   }
-};
+
+  error.classList.add("hidden");
+  weather.classList.add("hidden");
+
+  loading.classList.remove("hidden");
+
+  console.log("Kota yang dicari:", city);
+
+  setTimeout(() => {
+    loading.classList.add("hidden");
+
+    weather.innerHTML = `
+      <div class="weather-item">Kota: ${city}</div>
+      <div class="weather-item">Status: (dummy data)</div>
+    `;
+    weather.classList.remove("hidden");
+  }, 1000);
+});
